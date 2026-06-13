40f7cf9c5e8cb8f1aea45bdb4f2ba4308482e08e
Prayag1-tecj <prayag.raj.9887@gmail.com>
2026-06-14 01:12:09 +0530

    feat: created core backend applications

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,9 +37,26 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third Party Apps
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'corsheaders',
+    'drf_spectacular',
+
+    # Local Apps
+    'accounts',
+    'preferences',
+    'instagram',
+    'automation',
+    'analytics_app',
+    'core',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
