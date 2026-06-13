30d41c830e282fb03ded766fa34284e61a762453
Prayag1-tecj <prayag.raj.9887@gmail.com>
2026-06-14 03:18:34 +0530

    feat: created user profile model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="profile",
+    )
+
+    full_name = models.CharField(max_length=255)
+
+    onboarding_completed = models.BooleanField(
+        default=False
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.user.username
